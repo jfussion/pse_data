f7697639ad2742f3c3484fd198018bd0449937ea
Joseph Bonilla <bonillajeb@gmail.com>
2020-04-28 19:30:50 +0800

    fix update function

--- update_data.py
+++ update_data.py
@@ -1,9 +1,12 @@
 import io
+import os
 import sys
 import zlib
 
 import pandas as pd
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 from symbols import TICKERS
 
@@ -13,13 +16,22 @@ def update():
     headers = {"Referer": "https://www.pse.com.ph", "Host": "www.pse.com.ph"}
 
     for ticker in TICKERS:
-        print("fetching {}...".format(ticker))
-        params = {"method": "getAnyStockStockData", "symbol": ticker}
         path = "data/{}.csv".format(ticker)
 
-        r = requests.get(url, params=params, headers=headers)
-        raw = zlib.decompress(r.content)
+        params = {"method": "getAnyStockStockData", "symbol": ticker}
+        s = requests.Session()
+        retries = Retry(
+            total=5, backoff_factor=0.3, status_forcelist=[500, 502, 503, 504]
+        )
+        s.mount("http://", HTTPAdapter(max_retries=retries))
 
+        print("fetching {}...".format(ticker))
+        r = s.get(url, params=params, headers=headers, timeout=5)
+        if not r.content:
+            print("skipping {}...".format(ticker))
+            continue
+
+        raw = zlib.decompress(r.content)
         df = pd.read_csv(
             io.BytesIO(raw),
             parse_dates=[0],
@@ -27,12 +39,17 @@ def update():
             index_col="date",
         ).sort_index()
 
-        orig = pd.read_csv(path, index_col="date", parse_dates=["date"])
+        if not os.path.isfile(path):
+            print("creating {}...".format(path))
+            new = df
+        else:
+            print("updating {}...".format(ticker))
+            orig = pd.read_csv(path, index_col="date", parse_dates=["date"])
+            new = orig.combine_first(df)
 
-        print("updating {}...".format(ticker))
-        new = orig.combine_first(df)
         new["volume"] = new["volume"].apply(lambda x: int(x))
         new.to_csv(path)
 
 
-update()
+if __name__ == "__main__":
+    update()
