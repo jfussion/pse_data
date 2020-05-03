97a5d5409fb2da9222365b9262f4c6ad0aa6a536
Joseph Bonilla <bonillajeb@gmail.com>
2020-05-03 12:31:10 +0800

    add init and setup script

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .get_data import get_data
+from .update_data import update
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="PSEData",
+    version="0.1",
+    description="Accessing and updating data from PSE",
+    url="#",
+    author="Joseph Bonilla",
+    author_email="bonillajeb@gmail.com",
+    license="MIT",
+    packages=["pse_data"],
+    zip_safe=False,
+)
